5d0eacc571a84f4d652ed861931b4e3766f0d17d
RajendrasinhParmar <rajendra20791@gmail.com>
2015-12-19 18:23:40 +0530

    Add controllers for AVKit player to play video from url

--- AVPlayerDemo/ViewController.h
+++ AVPlayerDemo/ViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController : UIViewController
 
-
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender;
 @end
 
--- AVPlayerDemo/ViewController.m
+++ AVPlayerDemo/ViewController.m
@@ -24,4 +24,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    AVPlayerViewController *destination = segue.destinationViewController;
+    NSURL *videoUrl = [[NSURL alloc] initWithString:@"http://mdollarideas.com/swastik_webservices/uploads/testimonial_videos/6755v8.mp4"];
+    destination.player = [[AVPlayer alloc]initWithURL:videoUrl];
+    [destination.player play];
+}
+
 @end
