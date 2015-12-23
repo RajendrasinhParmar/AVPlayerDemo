1a849afe9da3b2c1b03353f93c2450fb525164de
RajendrasinhParmar <rajendra20791@gmail.com>
2015-12-23 11:38:23 +0530

    Add file to control AVPlayerViewController

---
+++ AVPlayerDemo/VideoPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoPlayerViewController.h
+//  AVPlayerDemo
+//
+//  Created by Rajendrasinh Parmar on 23/12/15.
+//  Copyright © 2015 Rajendrasinh Parmar. All rights reserved.
+//
+
+#import <AVKit/AVKit.h>
+
+@interface VideoPlayerViewController : AVPlayerViewController
+
+@end
