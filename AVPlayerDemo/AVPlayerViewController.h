d5fdecf20bc39db9ef19469279af1874e6cc7bbc
RajendrasinhParmar <rajendra20791@gmail.com>
2015-12-23 11:30:51 +0530

    Add new controller file to handle AVPlayerViewController

---
+++ AVPlayerDemo/AVPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVPlayerViewController.h
+//  AVPlayerDemo
+//
+//  Created by Rajendrasinh Parmar on 23/12/15.
+//  Copyright © 2015 Rajendrasinh Parmar. All rights reserved.
+//
+
+#import <AVKit/AVKit.h>
+
+@interface AVPlayerViewController : AVPlayerViewController
+
+@end
